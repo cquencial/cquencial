4e8a4d9b4dbf1849f1a9115c72055358f5412d8a
Jan Küster <jkuester@uni-bremen.de>
2018-08-20 15:13:16 +0200

    improved error log on validated method

--- app/imports/api/validatedmethod/ValidatedMethod.js
+++ app/imports/api/validatedmethod/ValidatedMethod.js
@@ -1,91 +1,90 @@
-import { Meteor } from 'meteor/meteor';
-import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { Meteor } from 'meteor/meteor'
+import { ValidatedMethod } from 'meteor/mdg:validated-method'
 
-import { hasUsers, isRegisteredUser } from '../accounts/accountUtils'
-import { UserUtils } from '../accounts/UserUtils';
+import { hasUsers, isRegisteredUser, hasRole } from '../accounts/server/accountUtils'
+import { CommonErrors } from '../errors/Errors'
 
 /**
  * Mixin tomake every validated method automatically check if the running user is registered.
  * @param options
  */
 const PermissionsMixin = function (options) {
-  const runFct = options.run;
-  options.run = function run(...args) {
-    const { userId } = this;
-    const exception = options.permission && options.permission(...args);
+  const runFct = options.run
+  options.run = function run (...args) {
+    const {userId} = this
+    const exception = options.permission && options.permission(...args)
 
     // user permission
     if (hasUsers() && !isRegisteredUser(userId)) {
-      if (!exception) throw new Meteor.Error('403', `${options.name}: ${CommonErrors.noPermission}`);
+      if (!exception) throw new Meteor.Error(...CommonErrors.permissionDenied())
     }
 
-    return runFct.call(this, ...args);
-  };
-  return options;
-};
+    return runFct.call(this, ...args)
+  }
+  return options
+}
 
 /**
  * Mixin to check permissions of execution by given roles.
+ * If no role is defined on the methos, no checks will be performed.
  * @param options
  */
 const RoleMixin = function (options) {
   if (options.role) {
-    const runFct = options.run;
-    options.run = function run(...args) {
-      const { userId } = this;
-      const { role } = options;
+    const runFct = options.run
+    options.run = function run (...args) {
+      const {userId} = this
+      const {role} = options
 
       // CHECK ROLES
-      if (!UserUtils.hasAtLeastRole(this.userId, role)) {
-        throw new Meteor.Error('403', CommonErrors.notInRole);
+      if (!hasRole(userId, role)) {
+        throw new Meteor.Error(...CommonErrors.permissionDenied())
       }
 
-      return runFct.call(this, ...args);
-    };
+      return runFct.call(this, ...args)
+    }
   }
 
-  return options;
-};
+  return options
+}
 
 /**
  * Mixin to allow injecing an error logger before rethrowing the original error.
- * @param methodDefinition
+ * @param options
  */
-const ErrorLogMixin = function (methodDefinition) {
+const ErrorLogMixin = function (options) {
   // OVERRIDE RUN
-  const originalRun = methodDefinition.run;
-  methodDefinition.run = function (...args) {
+  const originalRun = options.run
+  options.run = function (...args) {
     try {
-      return originalRun.call(this, ...args);
+      return originalRun.call(this, ...args)
     } catch (error) {
-      if (this.userId) {
-        const errorLogObj = {
+      if (options.log) {
+        options.log({
           userId: this.userId,
-          isClient: false,
-          code: error.code || 'undefined',
-          level: LogLevel.error.name,
-          errorInstance: error,
-        };
-        logAndReturnError(errorLogObj);
+          client: Meteor.isClient,
+          server: Meteor.isServer,
+          errorInstance: error
+        })
       }
-      throw error;
+      throw error
     }
-  };
-  return methodDefinition;
-};
+  }
+  return options
+}
 
 /**
  * Extend validated method and inject mixins.
  */
-export default ExtendedValidatedMethod = class ExtendedValidatedMethod extends ValidatedMethod {
-  constructor(methodDefinition) {
+export default class ExtendedValidatedMethod extends ValidatedMethod {
+  constructor (methodDefinition) {
     // ADD DEFAULT MIXINS
     if (Array.isArray(methodDefinition.mixins)) {
-      methodDefinition.mixins = methodDefinition.mixins.concat(PermissionsMixin, RoleMixin, ErrorLogMixin);
+      methodDefinition.mixins = methodDefinition.mixins.concat(PermissionsMixin, RoleMixin, ErrorLogMixin)
     } else {
-      methodDefinition.mixins = [PermissionsMixin, RoleMixin, ErrorLogMixin];
+      methodDefinition.mixins = [PermissionsMixin, RoleMixin, ErrorLogMixin]
     }
 
-    super(methodDefinition);
+    super(methodDefinition)
   }
-};
+}

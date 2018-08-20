25490f11ba2ec79a303a825448b2836a4f0ae50e
jankapunkt <jkuester@uni-bremen.de>
2018-08-20 14:20:35 +0200

    installscript clones packages instead of using submodules

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,15 @@
 PROJECT_ROOT=$(pwd)
 mkdir -p $PROJECT_ROOT/packages
 
+# clone each repo manually instead of
+# messing around with submodules for now
+cd $PROJECT_ROOT/packages
+
+git clone git@github.com:cquencial/meteor-bpmn-engine.git
+git clone git@github.com:cquencial/meteor-bpmn-instances.git
+git clone git@github.com:cquencial/meteor-bpmn-persistence.git
+git clont git@github.com:cquencial/meteor-bpmn-history.git
+git clone git@github.com:cquencial/meteor-bpmn-tasks.git
+
 cd $PROJECT_ROOT/src && meteor npm install
 cd $PROJECT_ROOT

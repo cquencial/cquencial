d9f9cfbb088e9ab4620ba82acf4aa272e17fca12
jankapunkt <jkuester@uni-bremen.de>
2018-08-29 15:34:14 +0200

    logout page implemented

--- app/imports/ui/pages/logout/logout.js
+++ app/imports/ui/pages/logout/logout.js
@@ -1,1 +1,15 @@
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
+import { Routes } from '../../../api/routes/Routes'
 import './logout.html'
+
+Template.logout.onCreated(function onLogoutCreated () {
+  Meteor.logout((err) => {
+    if (err) {
+      console.error(err)
+      // TODO show modal
+    } else {
+      Routes.to.login.go()
+    }
+  })
+})

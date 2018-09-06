af3bceab7bea015523a413a61432e9ea9f11a444
Jan Küster <jkuester@uni-bremen.de>
2018-09-06 16:54:09 +0200

    moved account utils

--- app/imports/api/accounts/server/methods.js
+++ app/imports/api/accounts/server/methods.js
@@ -1,12 +1,12 @@
 import ValidatedMethod from '../../validatedmethod/ValidatedMethod'
-import {Cquencial} from '../../cquencial/Cquencial'
-import {hasUsers} from './accountUtils'
+import { Cquencial } from '../../cquencial/Cquencial'
+import { hasUsers } from '../accountUtils'
 
 export const setupRequired = new ValidatedMethod({
   name: Cquencial.methods.setupRequired.name,
-  validate() {},
-  run() {
-    console.log("has uses ", hasUsers())
+  validate () {},
+  run () {
+    console.log('has uses ', hasUsers())
     return !hasUsers() // && !hasAdmins() // TODO use Roles
   }
 })

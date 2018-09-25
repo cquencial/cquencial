fdcf968ea0f07b14ea9020959fa9de6e5070d526
Jan Küster <jkuester@uni-bremen.de>
2018-09-25 10:03:06 +0200

    cleanup main files

--- app/client/main.js
+++ app/client/main.js
@@ -1,13 +1,1 @@
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
-
-// HELPERS
-import '../imports/ui/utils/templateHelpers'
-
-// STARTUP IMPORTS
-import '../imports/startup/both'
-import '../imports/startup/client'
-
-// TEMPLATE IMPORTS
-import '../imports/ui/layout/nav/nav'
 import './main.html'
--- app/server/main.js
+++ app/server/main.js
@@ -1,14 +1,2 @@
 import '../imports/startup/both'
 import '../imports/startup/server/'
-
-import {WebApp} from 'meteor/webapp'
-
-WebApp.connectHandlers.use('/hello', (req, res, next) => {
-  res.writeHead(200);
-  res.end('hello from ' + Meteor.release)
-})
-
-WebApp.connectHandlers.use('/', (req, res, next) => {
-  res.writeHead(400)
-  res.end()
-})
